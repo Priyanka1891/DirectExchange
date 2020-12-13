ce7f846fb12b2b558ae02e952f5b3c22dcf98750
Harshit Malwiya <harshitmalwiya94@gmail.com>
2020-12-13 04:31:25 -0800

    Pagination, Filter Changes

--- Frontend/directexchange/src/components/BrowseOffers.js
+++ Frontend/directexchange/src/components/BrowseOffers.js
@@ -8,10 +8,11 @@ import {
     Route,
     Link
 } from "react-router-dom";
-import { MailOutlined } from '@ant-design/icons';
+import { FieldStringOutlined, MailOutlined } from '@ant-design/icons';
 import { Typography } from 'antd';
 import UserHeader from './userHeader';
-
+import filter from '@mcabreradev/filter';
+import JwPagination from 'jw-react-pagination';
 
 const { Title } = Typography;
 
@@ -29,8 +30,13 @@ class BrowseOffers extends React.Component {
     constructor() {
         super();
         this.state = {
+            dstCurrency: null,
+            srcCurrency: null,
+            srcAmount: null,
+            pageOfItems: []
+        };
+        this.onChangePage = this.onChangePage.bind(this);
 
-        }
     }
 
     // Listen to the Firebase Auth state and set the local state.
@@ -42,7 +48,8 @@ class BrowseOffers extends React.Component {
                 console.log("Search Result : ", response.data);
                 if (response.data != undefined) {
                     this.setState({
-                        offers: response.data
+                        offers: response.data,
+                        backupArray: response.data,
                     });
                 } else {
 
@@ -58,131 +65,207 @@ class BrowseOffers extends React.Component {
         if (e) {
             this.setState({
                 sourcecurrency: e.toUpperCase()
-            }, () => {
-                this.getExchangeRate();
             })
+
         }
         else {
             this.setState({
                 sourcecurrency: ''
-            }, () => {
-                this.getExchangeRate();
             })
         }
-
+        this.filterOffers();
     }
 
     onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        }, () => {
+                this.filterOffers();
+        })
+        console.log('state', this.state);
+        //this.filterOffers();
     }
+    filterOffers() {
+        console.log("inside fite", this.state.offers);
+        console.log("inside fite", this.state);
+        var filteredArray = this.state.backupArray;
+        
+        if (this.state.destinationcurrency != null) {
+            filteredArray = filter(filteredArray, {
+                'destinationCurrency': this.state.destinationcurrency
+            });
+        }  
+        if (this.state.sourcecurrency != null) {
+            filteredArray = filter(filteredArray, {
+                'sourceCurrency': this.state.sourcecurrency
+            });
+        }
+        if (this.state.srcAmount != null) {
+            filteredArray = filter(filteredArray, {
+                'amountToRemitSourceCurrency': this.state.srcAmount
+            });
+        }
+        
+
+        console.log("inside fite", filteredArray);
+        this.setState({ offers: filteredArray });
+
+    }
+
     onDestinationCurrencyChange = (e) => {
         if (e) {
             this.setState({
                 destinationcurrency: e.toUpperCase()
             }, () => {
-                this.getExchangeRate();
+                    this.filterOffers();
             })
         }
         else {
             this.setState({
                 destinationcurrency: ''
             }, () => {
-                this.getExchangeRate();
+                    this.filterOffers();
             })
         }
-
+        // this.filterOffers();
+    }
+    onChangePage(pageOfItems) {
+        // update local state with new page of items
+        this.setState({ pageOfItems });
     }
 
     render() {
         return (
             <>
-            <div>
-            <div>
+                <div>
+                    <div>
                         <UserHeader selectedKey={['2']} />
                     </div>
-                <Card title="Browse Offers">
+                    <Title level={3}>Browse Offers</Title>
+                    <div >
+                        <Row >
+                            <Divider type="vertical" />
+                            <Col className="gutter-row" span={3}>
+                                <Form.Item
+                                    name="sourcecurrency"
+                                    label="Source Currency"
+                                    rules={[
+                                        {
+                                            required: false,
+                                        },
+                                    ]}
+                                >
+                                    <Select onChange={this.onSourceCurrencyChange} name="srcCurrency" style={{ "max-width": "57px" }}>
+                                        <Option value="EUR">EUR</Option>
+                                        <Option value="GBP">GBP</Option>
+                                        <Option value="INR">INR</Option>
+                                        <Option value="RMB">RMB</Option>
+                                        <Option value="USD">USD</Option>
+                                    </Select>
+                                </Form.Item>
 
-                    <Row gutter={{ xs: 8, xs: 8 }}>
-                        <Col className="gutter-row" span={4}>
-                            <Form.Item
-                                name="sourcecurrency"
-                                label="Source Currency"
-                                rules={[
-                                    {
-                                        required: false,
-                                    },
-                                ]}
-                            >
-                                <Select onChange={this.onSourceCurrencyChange}>
-                                    <Option value="EUR">EUR</Option>
-                                    <Option value="GBP">GBP</Option>
-                                    <Option value="INR">INR</Option>
-                                    <Option value="RMB">RMB</Option>
-                                    <Option value="USD">USD</Option>
-                                </Select>
-                            </Form.Item>
+                            </Col>
+                            <Divider type="vertical" />
+                            <Col className="gutter-row" span={4}>
+                                <Form.Item
+                                    name="destinationcurrency"
+                                    label="Destination Currency"
+                                    rules={[
+                                        {
+                                            required: false,
+                                        },
+                                    ]}
+                                >
+                                    <Select onChange={this.onDestinationCurrencyChange} name="dstCurrency" style={{ "max-width": "57px" }}>
+                                        <Option value="EUR">EUR</Option>
+                                        <Option value="GBP">GBP</Option>
+                                        <Option value="INR">INR</Option>
+                                        <Option value="RMB">RMB</Option>
+                                        <Option value="USD">USD</Option>
+                                    </Select>
+                                </Form.Item>
+                            </Col>
+                            <Divider type="vertical" />
+                            <Col className="gutter-row" span={5}>
+                                <Form.Item
+                                    name="Source Currency Amount"
+                                    label="Source Currency Amount"
+                                    rules={[
+                                        {
+                                            required: false,
+                                        },
+                                    ]}
+                                >
+                                    <Input
+                                        type="text"
+                                        value="1000"
+                                        name="srcAmount"
+                                        onChange={this.onChange}
+                                        style={{ width: 50 }}
+                                    />
+                                </Form.Item>
+                            </Col>
+                            <Divider type="vertical" />
+                            <Col className="gutter-row" span={5}>
+                                <Form.Item
+                                    name="Destination Currency Amount"
+                                    label="Destination Currency Amount"
+                                    rules={[
+                                        {
+                                            required: false,
+                                        },
+                                    ]}
+                                >
+                                    <Input
+                                        type="text"
+                                        value="1000"
+                                        name="destAmount"
+                                        onChange={this.onChange}
+                                        style={{ width: 50 }}
+                                    />
+                                </Form.Item>
+                            </Col>
+                        </Row>
 
-                        </Col>
-                        <Col className="gutter-row" span={4}>
-                            <Form.Item
-                                name="destinationcurrency"
-                                label="Destination Currency"
-                                rules={[
-                                    {
-                                        required: false,
-                                    },
-                                ]}
-                            >
-                                <Select onChange={this.onDestinationCurrencyChange}>
-                                    <Option value="EUR">EUR</Option>
-                                    <Option value="GBP">GBP</Option>
-                                    <Option value="INR">INR</Option>
-                                    <Option value="RMB">RMB</Option>
-                                    <Option value="USD">USD</Option>
-                                </Select>
-                            </Form.Item>
-                        </Col>
-                    </Row>
-
-
-
-
-
-                    {this.state.offers && this.state.offers.map((value, index) => {
-                        return <>
-                            <Card.Grid bordered={true} style={gridStyle}>
-                                <p><b>Offer ID : {value.id}
-                                    <Divider type="vertical" /></b>
+                    </div>
+                    <Card>
+                        {this.state.pageOfItems && this.state.pageOfItems.map((value, index) => {
+                            return <>
+                                <Card.Grid bordered={true} style={gridStyle}>
+                                    <p><b>Offer ID : {value.id}
+                                        <Divider type="vertical" /></b>
                         Offer Status : {value.offerStatus}
-                                </p>
-                                <Divider />
-                                <p><b>Amount</b> : {value.amountToRemitSourceCurrency}</p>
-                                <p><b>Exchange Rate</b> : {value.exchangeRate}</p>
-                                <Divider />
-                                <p>Source Country : {value.sourceCountry}</p>
-                                <p>Destination Country : {value.destinationCountry}</p>
-                                <p>Destination Currency : {value.destinationCurrency}</p>
-                                <Divider />
-                                <p>Counter Offer Allowed? : {value.allowCounterOffers}</p>
-                                <p>Split Allowed? : {value.allowSplitExchanges}</p>
-                                <p>Expiration Date : {value.expirationDate}</p>
+                                    </p>
+                                    <Divider />
+                                    <p><b>Amount</b> : {value.amountToRemitSourceCurrency}</p>
+                                    <p><b>Exchange Rate</b> : {value.exchangeRate}</p>
+                                    <Divider />
+                                    <p>Source Country : {value.sourceCountry}</p>
+                                    <p>Destination Country : {value.destinationCountry}</p>
+                                    <p>Destination Currency : {value.destinationCurrency}</p>
+                                    <Divider />
+                                    <p>Counter Offer Allowed? : {value.allowCounterOffers}</p>
+                                    <p>Split Allowed? : {value.allowSplitExchanges}</p>
+                                    <p>Expiration Date : {value.expirationDate}</p>
 
-                                {/* <p>{value.receivingAccountNumber}</p> */}
-                                <p>Bank : {value.receivingBankName}</p>
-                                <Divider orientation="left">User Details</Divider>
-                                <p>{value.user.name}<Divider type="vertical" /> <MailOutlined /> Ratings </p>
-                                <Divider dashed />
-                                <Link to={{
-                                    pathname: '/offer/details/',
-                                    state: value
-                                }}>
-                                    <Button type="primary">Show Offer Details</Button>
-                                </Link>
-                            </Card.Grid>
-                        </>
-                    })}
+                                    {/* <p>{value.receivingAccountNumber}</p> */}
+                                    <p>Bank : {value.receivingBankName}</p>
+                                    <Divider orientation="left">User Details</Divider>
+                                    <p>{value.user.name}<Divider type="vertical" /> <MailOutlined /> Ratings </p>
+                                    <Divider dashed />
+                                    <Link to={{
+                                        pathname: '/offer/details/',
+                                        state: value
+                                    }}>
+                                        <Button type="primary">Show Offer Details</Button>
+                                    </Link>
+                                </Card.Grid>
+                            </>
+                        })}
 
-                </Card>
-                <Pagination defaultCurrent={1} total={50} />
+                    </Card>
+                    <JwPagination items={this.state.offers} onChangePage={this.onChangePage} />
+
                 </div>
             </>
         )
